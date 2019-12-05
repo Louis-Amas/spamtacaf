14985ff598e4aca383aa2cf18b90fbe79f42d65f
Hasnaa-Elalaoui <hasnaa.elalaoui-m'hamdy@etu.univ-coted'azur.fr>
2019-12-05 21:40:42 +0100

    Best commit ever

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Spam" src="./assets/spam.jpeg">
+    <img alt="Ta" src="./assets/ta.png">
+    <img alt="CAF" src="./assets/caf.png">
+    <HelloWorld msg="SPAM TA CAF"/>
   </div>
 </template>
 
