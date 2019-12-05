f34af15584e8085548ff732d69c55427086d3b8b
Pierre-Antoine Boulat <pierre-antoine.boulat@etu.unice.fr>
2019-12-05 21:39:58 +0100

    update home component

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,18 @@
 <template>
-  <div id="app">
-    <v-container grid-list-xs>
-
-      <!--       <HelloWorld msg="Welcome to Your Vue.js App" /> -->
-      <v-layout column>
-        <v-layout row wrap>
-          <v-flex md12>
-            <img alt="Vue logo" src="./assets/logo.png">
-          </v-flex>
-        </v-layout>
-        <v-layout row wrap justify-space-around>
-          <v-flex md4>
-            <v-btn flat color="success">Du sale</v-btn>
-          </v-flex>
-          <v-flex md4>
-            <v-btn flat color="error">Du tres sale</v-btn>
-          </v-flex>
-        </v-layout>
-      </v-layout>
-    </v-container>
-  </div>
+  <v-app>
+    <v-content>
+      <Home />
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-  /*   import HelloWorld from './components/HelloWorld.vue'
-   */
+  import Home from './components/Home.vue'
+
   export default {
     name: 'app',
     components: {
-      /* HelloWorld */
+      Home
     }
   }
 </script>
