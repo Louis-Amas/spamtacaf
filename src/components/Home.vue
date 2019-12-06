406e8dd84a979fefe8228c2ddda8e8d1a28416d1
Julien <julien.molinier@etu.univ-cotedazur.fr>
2019-12-06 04:12:48 +0100

    ++

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -18,7 +18,113 @@
                     </v-row>
                 </v-parallax>
             </section>
-
+            <section>
+                <v-layout justify-center ma-10 row wrap>
+                    <v-flex md8>
+                        <v-lazy :options="{threshold: .5}" transition="slide-x-transition" v-model="isActive">
+                            <v-card>
+                                <v-layout row wrap>
+                                    <v-flex md5>
+                                        <v-container pb-0 pt-0>
+                                            <v-img src="../assets/precarite.jpg"></v-img>
+                                        </v-container>
+                                    </v-flex>
+                                    <v-flex align-self-center md7 pa-8>
+                                        <v-card-title class="layout justify-center" primary-title>La précarité étudiante
+                                        </v-card-title>
+                                        <v-card-text class="text-justify">Près de 10% des étudiant·e·s sont
+                                            confronté·e·s à
+                                            des situations aigües de précarité qui augmentent
+                                            leurs risques de décrochage et compromettent leurs chances de réussite dans
+                                            l’enseignement
+                                            supérieur. Beaucoup abandonnent leurs études, ou subissent des redoublements
+                                            et
+                                            réorientations
+                                            multiples.
+                                        </v-card-text>
+                                    </v-flex>
+                                </v-layout>
+                            </v-card>
+                        </v-lazy>
+                    </v-flex>
+                </v-layout>
+                <v-layout justify-center ma-10 row wrap>
+                    <v-flex md8>
+                        <v-lazy :options="{threshold: .5}" transition="slide-x-transition" v-model="isActive">
+                            <v-card>
+                                <v-layout row wrap>
+                                    <v-flex align-self-center md7 pa-8>
+                                        <v-card-title class="layout justify-center" primary-title>Un éparpillement des
+                                            ressources
+                                        </v-card-title>
+                                        <v-card-text class="text-justify">Les services que proposent les établissements
+                                            universitaires, eux-mêmes gérés par des
+                                            administrations différentes, dans des lieux
+                                            multiples, avec des horaires différents : bourses,
+                                            aides exceptionnelles et exonérations, santé,
+                                            logement, orientation, aménagements d’étude...<br>
+                                            Les droits auprès de diverses institutions :
+                                            tarifications solidaires dans les transports, CAF,
+                                            Couverture Mutuelle Universelle...<br>
+                                            Le soutien apporté par les associations :
+                                            distributions alimentaires, soutien académique,
+                                            domiciles intergénérationnels, ateliers-vélo,
+                                            ressourceries...
+                                        </v-card-text>
+                                    </v-flex>
+                                    <v-flex md5>
+                                        <v-container pb-0 pt-0>
+                                            <v-img src="../assets/maze.jpg"></v-img>
+                                        </v-container>
+                                    </v-flex>
+                                </v-layout>
+                            </v-card>
+                        </v-lazy>
+                    </v-flex>
+                </v-layout>
+                <v-layout justify-center ma-10 row wrap>
+                    <v-flex md8>
+                        <v-lazy :options="{threshold: 0.5}" transition="slide-x-transition" v-model="isActive">
+                            <v-card>
+                                <v-layout row wrap>
+                                    <v-flex md5>
+                                        <v-container pb-0 pt-0>
+                                            <v-img src="../assets/harry.jpg"></v-img>
+                                        </v-container>
+                                    </v-flex>
+                                    <v-flex align-self-center md7 pa-8>
+                                        <v-card-title class="layout justify-center" primary-title>Une fragilisation
+                                            sociale
+                                            et morale
+                                        </v-card-title>
+                                        <v-card-text class="text-justify">Déficit du soutien familial, sentiment de
+                                            vulnérabilité et de honte, déficit de maîtrise des ‘codes’ administratifs,
+                                            sociaux… pour comprendre et se conformer aux
+                                            procédures en vigueur, non-recours aux droits : désillusion, perte de
+                                            confiance, épuisement moral
+                                        </v-card-text>
+                                    </v-flex>
+                                </v-layout>
+                            </v-card>
+                        </v-lazy>
+                    </v-flex>
+                </v-layout>
+            </section>
+            <section>
+                <v-parallax height="380" src="../assets/book.jpg">
+                    <v-layout align-center column justify-center>
+                        <div class="headline black--text mb-4 text-center">Notre mission</div>
+                        <em class="black--text">Augmenter le
+                            pouvoir d’agir étudiant et institutionnel</em>
+                        <div class="black--text mt-4 text-center">Faciliter l’identification des ressources et la
+                            circulation entre les informations<br>Favoriser la transmission pair-à-pair, par des
+                            étudiants (experts) en direction d’étudiants (plus
+                            novices)<br>Développer de nouveaux canaux de
+                            communication<br>
+                        </div>
+                    </v-layout>
+                </v-parallax>
+            </section>
             <section>
                 <v-layout align-center class="my-12" column wrap>
                     <v-flex class="my-4" sm4 xs12>
@@ -96,7 +202,7 @@
                 <v-parallax height="380" src="../assets/book.jpg">
                     <v-layout align-center column justify-center>
                         <div class="headline black--text mb-4 text-center">Web development has never been easier</div>
-                        <em class="grey--text">Kick-start your application today</em>
+                        <em class="black--text">Kick-start your application today</em>
                         <v-btn @click="goToLogin" class="mt-12" color="primary" dark large>
                             Get Started
                         </v-btn>
